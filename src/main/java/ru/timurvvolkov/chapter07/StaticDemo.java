bbf5695da6f4c31dc23208d36e4b9f880e26d556
timurvvolkov <timurvvolkov@gmail.com>
2020-11-27 16:40:31 +0300

    Page 206

---
+++ src/main/java/ru/timurvvolkov/chapter07/StaticDemo.java
@@ -0,0 +1,10 @@
+package ru.timurvvolkov.chapter07;
+
+public class StaticDemo {
+    static int a = 42;
+    static int b = 99;
+
+    static void callme() {
+        System.out.println("a = " + a);
+    }
+}
