7c5cf14513e7eceb9efcbefcb6f262ec7bf91b5d
timurvvolkov <timurvvolkov@gmail.com>
2020-12-26 13:35:02 +0300

    Page 653

---
+++ src/main/java/com/timurvvolkov/chapter19/ArrayListDemo.java
@@ -0,0 +1,23 @@
+package com.timurvvolkov.chapter19;
+
+import java.util.ArrayList;
+
+class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<>();
+        System.out.println("Начальный размер списочного массива al: " + al.size());
+        al.add("C");
+        al.add("A");
+        al.add("E");
+        al.add("B");
+        al.add("D");
+        al.add("F");
+        al.add(1, "A2");
+        System.out.println("Размер списочного массива al после ввода элементов: " + al.size());
+        System.out.println("Содержимое списочного массива al:" + al);
+        al.remove("F");
+        al.remove(2);
+        System.out.println("Размер списочного массива al после удаления элементов: " + al.size());
+        System.out.println("Содержимое списочного массива al:" + al);
+    }
+}
