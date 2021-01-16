d596b2a02178778d13f1e7a3e85cb02107c4f331
timurvvolkov <timurvvolkov@gmail.com>
2021-01-16 12:53:48 +0300

    dmdev 18.3

---
+++ src/main/java/com/timurvvolkov/dmdev/Lesson18_3/PairUtil.java
@@ -0,0 +1,12 @@
+package com.timurvvolkov.dmdev.Lesson18_3;
+
+public final class PairUtil {
+    private PairUtil() {
+    }
+
+    public static <K, V> Pair<V, K> swap(Pair<K, V> source) {
+        V second = source.getSecond();
+        K first = source.getFirst();
+        return new Pair<>(second, first);
+    }
+}
