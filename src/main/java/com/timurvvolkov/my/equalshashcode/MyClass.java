b6e61d3a1a0c7258c075d1538b82f96e21d2ee0a
timurvvolkov <timurvvolkov@gmail.com>
2021-01-23 21:06:31 +0300

    EqualsHashcode

---
+++ src/main/java/com/timurvvolkov/my/equalshashcode/MyClass.java
@@ -0,0 +1,35 @@
+package com.timurvvolkov.my.equalshashcode;
+
+import java.util.Objects;
+
+public class MyClass {
+    private int a;
+    private int b;
+
+    public MyClass(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyClass myClass = (MyClass) o;
+        return a == myClass.a &&
+                b == myClass.b;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
+    public static void main(String[] args) {
+        MyClass myClass1 = new MyClass(1, 2);
+        MyClass myClass2 = new MyClass(2, 1);
+        System.out.println(myClass1.hashCode());
+        System.out.println(myClass2.hashCode());
+        System.out.println(myClass1.equals(myClass2));
+    }
+}
