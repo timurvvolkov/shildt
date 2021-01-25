e4116cdf58544c4cb363aa458363eeb54885aed3
timurvvolkov <timurvvolkov@gmail.com>
2021-01-25 13:20:32 +0300

    Page 872

---
+++ src/main/java/com/timurvvolkov/chapter22/ExplicitChannelRead2.java
@@ -0,0 +1,33 @@
+package com.timurvvolkov.chapter22;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
+public class ExplicitChannelRead2 {
+    public static void main(String[] args) {
+        int count;
+
+        try (SeekableByteChannel fChan = Files.newByteChannel(Paths.get("test.txt"))) {
+            ByteBuffer mBuf = ByteBuffer.allocate(128);
+
+            do {
+                count = fChan.read(mBuf);
+                if (count != -1) {
+                    mBuf.rewind();
+                    for (int i = 0; i < count; i++) {
+                        System.out.print((char) mBuf.get());
+                    }
+                }
+            } while (count != -1);
+            System.out.println();
+        } catch (InvalidPathException e) {
+            System.out.println("Ошибка указания пути " + e);
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода " + e);
+        }
+    }
+}
---
+++ src/main/java/com/timurvvolkov/chapter22/MappedChannelRead.java
@@ -0,0 +1,25 @@
+package com.timurvvolkov.chapter22;
+
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
+public class MappedChannelRead {
+    public static void main(String[] args) {
+        try (FileChannel fChan = (FileChannel) Files.newByteChannel(Paths.get("test.txt"))) {
+            long fSize = fChan.size();
+            MappedByteBuffer mBuf = fChan.map(FileChannel.MapMode.READ_ONLY, 0, fSize);
+            for (int i = 0; i < fSize; i++) {
+                System.out.print((char) mBuf.get());
+            }
+            System.out.println();
+        } catch (InvalidPathException e) {
+            System.out.println("Ошибка указания пути " + e);
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода " + e);
+        }
+    }
+}
