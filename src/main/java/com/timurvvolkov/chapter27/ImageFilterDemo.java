f8f8e592b753e76ce730d388f8a142c9ee7703cd
timurvvolkov <timurvvolkov@gmail.com>
2021-02-28 21:17:17 +0300

    Page 1057

---
+++ src/main/java/com/timurvvolkov/chapter27/ImageFilterDemo.java
@@ -0,0 +1,105 @@
+package com.timurvvolkov.chapter27;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class ImageFilterDemo extends Frame implements ActionListener {
+    Image img;
+    PlugInFilter pif;
+    Image fimg;
+    Image curImg;
+    LoadedImage lim;
+    Label lab;
+    Button reset;
+
+    String[] filters = {"Grayscale", "Invert", "Contrast", "Blur", "Sharpen"};
+
+    public ImageFilterDemo() {
+        Panel p = new Panel();
+        add(p, BorderLayout.SOUTH);
+
+        reset = new Button("Reset");
+        reset.addActionListener(this);
+        p.add(reset);
+
+        for (String fstr : filters) {
+            Button b = new Button(fstr);
+            b.addActionListener(this);
+            p.add(b);
+        }
+
+        lab = new Label("");
+        add(lab, BorderLayout.NORTH);
+
+        try {
+            File imageFile = new File("Lilies.jpg");
+
+            img = ImageIO.read(imageFile);
+        } catch (IOException exc) {
+            System.out.println("Cannot load image file.");
+            System.exit(0);
+        }
+
+        lim = new LoadedImage(img);
+        add(lim, BorderLayout.CENTER);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        String a = "";
+
+        try {
+            a = ae.getActionCommand();
+            if (a.equals("Reset")) {
+                lim.set(img);
+                lab.setText("Normal");
+            } else if (a.equals("Grayscale")) {
+                pif = new Grayscale();
+                fimg = pif.filter(this, img);
+                lim.set(fimg);
+                lab.setText("Filtered: " + a);
+            } else if (a.equals("Invert")) {
+                pif = new Invert();
+                fimg = pif.filter(this, img);
+                lim.set(fimg);
+                lab.setText("Filtered: " + a);
+            } else {
+                pif = (PlugInFilter) (Class.forName(a)).getConstructor().newInstance();
+                fimg = pif.filter(this, img);
+                lim.set(fimg);
+                lab.setText("Filtered: " + a);
+            }
+            repaint();
+        } catch (ClassNotFoundException e) {
+            lab.setText(a + " not found");
+            lim.set(img);
+            repaint();
+        } catch (InstantiationException e) {
+            lab.setText("couldn't new " + a);
+        } catch (IllegalAccessException e) {
+            lab.setText("no access: " + a);
+        } catch (NoSuchMethodException | InvocationTargetException e) {
+            lab.setText("Filter creation error: " + a);
+        }
+    }
+
+    public static void main(String[] args) {
+        ImageFilterDemo appwin = new ImageFilterDemo();
+        appwin.setSize(new Dimension(420, 420));
+        appwin.setTitle("ImageFilterDemo");
+        appwin.setVisible(true);
+    }
+}
---
+++ src/main/java/com/timurvvolkov/chapter27/PlugInFilter.java
@@ -0,0 +1,7 @@
+package com.timurvvolkov.chapter27;
+
+import java.awt.*;
+
+interface PlugInFilter {
+    Image filter(Frame f, Image in);
+}
